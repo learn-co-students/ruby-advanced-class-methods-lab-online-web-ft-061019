3a4399438813f7ebed1e010dfc63a63f69af8fde
Jeremiah Rodden <jrodden1@gmail.com>
2019-06-18 09:21:02 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,44 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = self.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.create
+    new_song.name = name
+    new_song
+  end
+
+  def self.find_by_name(name)
+    is_found = false
+    self.all.each do |song|
+      if song.name == name 
+        is_found = song
+      end
+    end
+    is_found
+  end
+
+  def self.find_or_create_by_name(name)
+    is_found = self.find_by_name(name)
+    binding.pry
+    if is_found == false 
+      action = self.create_by_name(name) 
+    else 
+      action = is_found
+    end
+    action
+  end
+
+
 end
