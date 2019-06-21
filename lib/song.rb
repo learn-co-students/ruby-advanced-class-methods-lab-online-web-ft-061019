ebbc6983c2af7fc42ff040fc7e50e6e7eb1e7b59
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-21 09:47:35 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,70 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+  def self.find_or_create_by_name(title)
+    song = self.find_by_name(title)
+    if song.nil?
+      self.create_by_name(title)
+    else
+      song
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort { |a, b| a.name <=> b.name }
+  end
+
+  def self.new_from_filename(filename)
+    # Split filename into array
+    file = filename.split("-")
+    # Get artist name and title
+    artist_name = file[0].strip
+    
+    title = file[1].strip.split(".")[0]
+    # Initialize a song
+    song = self.new_by_name(title)
+    # an artist name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+        # Split filename into array
+        file = filename.split("-")
+        # Get artist name and title
+        artist_name = file[0].strip
+        title = file[1].strip.split(".")[0]
+        # Initialize a song
+        song = self.create_by_name(title)
+        song.artist_name = artist_name
+        song
+    
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
+
