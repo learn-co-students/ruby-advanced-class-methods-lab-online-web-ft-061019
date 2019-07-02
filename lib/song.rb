bb38d10654bc5f105cd4d8bff07a77badefa324b
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-02 20:54:54 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,6 @@ class Song
   def self.create
     song = self.new 
     song.save 
-    @@all << song
     song
   end 
   def self.all
@@ -15,7 +14,6 @@ class Song
   def self.create_by_name(name)
     songName = self.create
     songName.name = name
-    songName.save 
     songName
   end 
   def self.new_by_name(string_fed_in_by_test)
@@ -27,24 +25,22 @@ class Song
      self.all.detect {|song| song.name == find_name}
   end 
   def self.find_or_create_by_name(string_name)
-    did_i_find_it = self.all.detect {|song| song.name == string_name}
-    if did_i_find_it == nil
-      songObj = self.new 
-      songObj.name = string_name
-      songObj.save 
-      songObj
-    else
-      did_i_find_it
-    end
+    self.find_by_name(string_name) || self.create_by_name(string_name)
   end 
   def self.alphabetical
-    self.all.sort_by { |song| song.name}
+    @@all.sort_by { |song| song.name}
    end 
-  def self.new_from_filename(new_file)
-    
+  def self.new_from_filename(file_name)
+    new_song = self.new
+    new_song.name = file_name.split(" - ")[1].split(".")[0]
+    new_song.artist_name = file_name.split(" - ")[0]
+    new_song
   end 
-  def self.create_from_filename(name)
-    
+  def self.create_from_filename(artist_name)
+     new_song = self.new
+    new_song.name = artist_name.split(" - ")[1].split(".")[0]
+    new_song.artist_name = artist_name.split(" - ")[0]
+    @@all << new_song
   end 
   def self.destroy_all
     @@all.clear
